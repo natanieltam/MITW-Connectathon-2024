ff88776f61b82bcb6f7ab705a8c967894d2d9830
natanieltam <94886538+natanieltam@users.noreply.github.com>
2024-08-25 21:59:56 +0700

    feat: adding form page, add style using css

--- App/src/App.js
+++ App/src/App.js
@@ -1,32 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const App = ({onSave, patient}) => {
+const App = ({ onSave, patient }) => {
   const [name, setName] = useState(patient?.name?.[0]?.given?.[0] || '');
   const [family, setFamily] = useState(patient?.name?.[0]?.family || '');
   const [gender, setGender] = useState(patient?.gender || '');
   const [birthDate, setBirthDate] = useState(patient?.birthDate || '');
 
   const handleSubmit = (e) => {
-    
-  }
+    e.preventDefault();
+    onSave({ name, family, gender, birthDate });
+  };
 
-  return(
+  return (
     <form onSubmit={handleSubmit}>
       <h1>FHIR App</h1>
       <div>
         <label>First Name: </label>
-        <input type="text" value={name} onChange={(e) => e.target.value}></input>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
         <label>Last Name: </label>
-        <input type="text" value={family} onChange={(e) => e.target.value}></input>
+        <input
+          type="text"
+          value={family}
+          onChange={(e) => setFamily(e.target.value)}
+        />
       </div>
       <div>
         <label>Gender: </label>
-        <select value={gender} onChange={(e) => e.target.value}>
-        <option value="male">Male</option>
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
           <option value="unknown">Unknown</option>
@@ -34,13 +42,15 @@ const App = ({onSave, patient}) => {
       </div>
       <div>
         <label>Birth Date:</label>
-        <input type="date" value={birthDate} onChange={(e) => e.target.value}/>
+        <input
+          type="date"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+        />
       </div>
       <button type="submit">Save Data</button>
     </form>
   );
-}
-
-
+};
 
 export default App;
